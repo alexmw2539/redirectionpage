8039fe387a7a7f3465d6df4c83f7c6a13eb511f6
alexmw2539 <59776902+alexmw2539@users.noreply.github.com>
2023-12-13 17:47:48 -0700

    Add files via upload

---
+++ redirection_page.js
@@ -0,0 +1,8 @@
+setTimeout(function() {
+    let x = document.getElementById("password").value;
+    if (x =="Aw84489202092008mmgim") {
+        {window.location = "https://gx.games/games/pc8f0q/dogfight-night/"}
+    } else {
+        {window.location = "https://sh1mmer.glitch.me"}
+    }
+}, 10000);
